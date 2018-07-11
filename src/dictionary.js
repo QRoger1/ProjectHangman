71c6070f7c21d10ba5e86a93c442eff81f5f4810
Administrator <Administrator@MVWINDOWS>
2018-07-11 19:03:45 -0400

    Control de niveles de juego

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,13 +1,54 @@
 const readWords = require('./word-reader.js')
 
 class Dictionary {
-	static getWord() {
-		return readWords({path:'./assets/es-ES.dic'})
-			.then(totalWords => {
-			  const index =  Math.floor(Math.random() * (totalWords.length + 1));
-			  return totalWords[index];
-			})
-	}
+  static accentedWord(word) {
+    let expression = /^[a-zA-Z]*$/;
+    if (expression.exec(word) === null) {
+      return true;
+    }
+    return false;
+  }
+
+  static getWord(difficulty = null, includePunctuation) {
+    return readWords({
+        path: './assets/es-ES.dic'
+      })
+      .then(totalWords => {
+        const index = Math.floor(Math.random() * (totalWords.length + 1));        
+        if (difficulty === null) {
+           return totalWords[index];
+        }
+        for (var i = index; i < totalWords.length; i++) {
+          let isAccented = Dictionary.accentedWord(totalWords[i]);          
+          if (`${isAccented}` === `${includePunctuation}`) {            
+            if (difficulty === 'hard' && totalWords[i].length > 8) {
+              return totalWords[i];
+            }
+            if (difficulty === 'easy' && totalWords[i].length > 5 && totalWords[i].length <= 8) {
+              return totalWords[i];
+            }
+            if (difficulty === 'super-easy' && totalWords[i].length <= 5) {
+              return totalWords[i];
+            }
+          }
+        }
+        for (var i = index; i > 0; i--) {
+          let isAccented = Dictionary.accentedWord(totalWords[i]);          
+          if (`${isAccented}` === `${includePunctuation}`) {
+            if (difficulty === 'hard' && totalWords[i].length > 8) {
+              return totalWords[i];
+            }
+            if (difficulty === 'easy' && totalWords[i].length > 5 && totalWords[i].length <= 8) {
+              return totalWords[i];
+            }
+            if (difficulty === 'super-easy' && totalWords[i].length <= 5) {
+              return totalWords[i];
+            }
+          }
+        }
+        return 'ERROR';
+      })
+  }
 }
 
-module.exports = Dictionary
+module.exports = Dictionary;
--- src/word-reader.js
+++ src/word-reader.js
@@ -18,15 +18,4 @@ function readWords ({path = ''} = {}) {
 	})
 }
 
-function WriteObjectToJson({path = ''} = {}, dataGames){
-	let dataGame = JSON.stringify(dataGames);  	
-	 return new Promise(function(resolve, reject) {
-            fs.writeFile(path, JSON.stringify(dataGame), function(err) {
-               if (err) reject(err);
-               else resolve(dataGame);
-            });
-    });  
-}
-
-module.exports = readWords;
-module.exports.WriteObjectToJson = WriteObjectToJson;
+module.exports = readWords
